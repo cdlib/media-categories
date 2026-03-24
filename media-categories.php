8c114807f33001b0c17d15477ed3ca508e8a90a1
Eric Satzman <7455691+esatzman@users.noreply.github.com>
2026-03-24 14:31:48 -0700

    Switch project license to GPL-2.0-or-later

--- media-categories.php
+++ media-categories.php
@@ -6,8 +6,8 @@
  * Version:     1.0.1
  * Author:      Eric Satzman
  * Update URI:  https://github.com/ericsatzman/media-categories
- * License:     MIT
- * License URI: https://opensource.org/licenses/MIT
+ * License:     GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: media-categories
  *
  * @package MediaCategories
